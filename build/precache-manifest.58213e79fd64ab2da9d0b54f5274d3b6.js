7e0df1fa4f39e72223c29432548f36b1ec972586
Tom <tom@MacBook-Air.local>
2020-03-06 13:22:34 +0000

    Include frontend code

---
+++ build/precache-manifest.58213e79fd64ab2da9d0b54f5274d3b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de03fb5c090884f2987843585a0751e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34a703c3510ac89bdd69",
+    "url": "/static/css/main.140ed08d.chunk.css"
+  },
+  {
+    "revision": "f14cff163cafd844c468",
+    "url": "/static/js/2.7f55ce51.chunk.js"
+  },
+  {
+    "revision": "8c7fe4041954829e8f9a44c5426cd717",
+    "url": "/static/js/2.7f55ce51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a703c3510ac89bdd69",
+    "url": "/static/js/main.f953c3f6.chunk.js"
+  },
+  {
+    "revision": "78936f68f28d1af5f782",
+    "url": "/static/js/runtime-main.16af93b5.js"
+  }
+]);
